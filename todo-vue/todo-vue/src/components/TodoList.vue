0dcd29afe1320f02bef42154e233ee28bb683ef2
unknown <jr435@naver.com>
2019-11-18 15:20:59 +0900

    Vue | TodoList props todos

--- todo-vue/todo-vue/src/components/TodoList.vue
+++ todo-vue/todo-vue/src/components/TodoList.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="todo-list">
       <h2>TODO</h2>
+      <ul>
+        <li v-for="todo in todos" :key="todo.id">
+          {{ todo.title }}
+        </li>
+      </ul>
   </div>
 </template>
 
 <script>
 export default {
   name : 'TodoList',
-  
+  props : {
+    //
+    todos : {
+      type: Array,
+      required: true
+    }
+  }
 }
 </script>
 

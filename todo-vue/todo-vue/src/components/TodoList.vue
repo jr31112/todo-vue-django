98306e467db656d4ca150b53c95fc7f184bd0136
unknown <jr435@naver.com>
2019-11-20 09:14:19 +0900

    Vue | DeleteTodo, UpdateTodo

--- todo-vue/todo-vue/src/components/TodoList.vue
+++ todo-vue/todo-vue/src/components/TodoList.vue
@@ -3,13 +3,16 @@
       <h2>TODO</h2>
       <ul>
         <li v-for="todo in todos" :key="todo.id">
+          <input type="checkbox" v-model="todo.is_completed" @change='clickCheck(todo)'>
           {{ todo.title }}
+          <button @click.prevent='deleteTodo(todo)'>X</button>
         </li>
       </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name : 'TodoList',
   props : {
@@ -17,6 +20,47 @@ export default {
     todos : {
       type: Array,
       required: true
+    }
+  },
+  methods : {
+    deleteTodo(todo){
+      this.$session.start()
+      const token = this.$session.get('jwt')
+      const options = {
+        headers: {
+          Authorization : `JWT ${token}` // JWT 뒤 공백 있음
+        }
+      }
+      axios.delete(`http://127.0.0.1:8000/api/v1/todos/${todo.id}/`,options)
+      .then(Response=>{
+        console.log(Response)
+        // const target = this.todos.find(el => {
+        //   return el === todo
+        //   })
+        const idx = this.todos.indexOf(todo)
+        if (idx > -1) {
+          this.todos.splice(idx, 1)
+        }
+      })
+      .catch(error => {
+      console.log(error)
+    })
+    },
+    clickCheck(todo){
+      this.$session.start()
+      const token = this.$session.get('jwt')
+      const options = {
+        headers: {
+          Authorization : `JWT ${token}` // JWT 뒤 공백 있음
+        }
+      }
+      axios.put(`http://127.0.0.1:8000/api/v1/todos/${todo.id}/`, todo, options)
+      .then(Response =>{
+        console.log(Response)
+      })
+      .catch(error=>{
+        console.log(error)
+      })
     }
   }
 }
